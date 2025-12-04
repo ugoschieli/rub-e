d738a0f14d4a95a33c64115dbd5aa710574217ff
luckonis <lucas.koenig@epitech.eu>
2025-12-04 15:32:53 +0100

    Update  All Project with thenew layout

--- asset-software/src/app/all-projects/layout.tsx
+++ asset-software/src/app/all-projects/layout.tsx
@@ -12,28 +12,20 @@ import {
 
 export default function AllProjectsLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <div className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+    <>
+      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
         <div className="flex items-center gap-2 px-4">
-          <SidebarTrigger className="-ml-1 text-white" />
-          <Separator
-            orientation="vertical"
-            className="mr-2 data-[orientation=vertical]:h-4"
-          />
           <Breadcrumb>
             <BreadcrumbList>
               <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbPage >
-                  All Project
-                </BreadcrumbPage>
+                <BreadcrumbPage>All Projects</BreadcrumbPage>
               </BreadcrumbItem>
+              <BreadcrumbSeparator />
             </BreadcrumbList>
           </Breadcrumb>
         </div>
-      </div>
-      <body >
-        <main>{children}</main>
-      </body>
-    </html>
+      </header>
+      <main>{children}</main>
+    </>
   );
 }
